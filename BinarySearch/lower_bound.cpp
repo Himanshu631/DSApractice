f109d84179ef6bbf44e916f06e773fe179988add
Himanshu Paul <himanshupauljsr@@gmail.com>
2023-06-02 16:21:20 +0530

    updated-lower-bound

--- BinarySearch/lower_bound.cpp
+++ BinarySearch/lower_bound.cpp
@@ -6,24 +6,22 @@ int main()
 {
     // find the lower bound - return the index if not possible return -1
     // lower bound is the lowest index such that arr[i]>=num 
-    
     int arr[12] = {1,2,3,4,7,9,10,11,12,15,16,20};
     
-    int num = 0;
+    int num = 10;
     
     int low = 0;
     int high = 11;
-    int lb = high;
-    while(low < high){
+    int lb = -1;
+    while(low <= high){
         int mid = low + (high-low)/2;
         
-        if(arr[mid]<=num){
-            low = mid+1;
+        if(arr[mid]>=num){
+            lb = high;
+            high=mid-1;
         }
         else
-            if(arr[mid]>num)
-                high=mid-1;
-        lb = high;    
+            low = mid+1;
     }
     
     cout<<lb<<endl;
